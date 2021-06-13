daefada8b52ee274dd3f1bb92c3368fa3936c196
dream_Faunus <cappu159@gmail.com>
2021-06-13 21:48:48 +0900

    Loading spinning bar

--- src/context/context.js
+++ src/context/context.js
@@ -13,8 +13,64 @@ const GithubProvider = ({ children }) => {
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
 
+  // request loading
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // error
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const searchGithubUser = async (user) => {
+    toggleError();
+    setIsLoading(true);
+    const response = await axios(`${rootUrl}/users/${user}`) //
+      .catch((error) => console.log(error));
+    if (response) {
+      setGithubUser(response.data);
+      // more logic error
+    } else {
+      toggleError(true, "There is no user with that username");
+    }
+    checkRequests();
+    setIsLoading(false);
+  };
+
+  // check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+
+        setRequests(remaining);
+        if (remaining === 0) {
+          // throw an error
+          toggleError(true, "sorry, you have exeeded your houly rate limit!!");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+
+  //error
+  useEffect(checkRequests, []);
+
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+        isLoading,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
